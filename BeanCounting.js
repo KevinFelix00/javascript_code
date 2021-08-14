940a068cfc573601f3c8eca00703e149d0879b07
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-08-14 23:18:24 +0100

    Update BeanCounting.js

--- BeanCounting.js
+++ BeanCounting.js
@@ -1,16 +1,15 @@
 /* Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase "B" characters there are in the string.
  Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted(rather than counting only uppercase "B" characters). Rewrite countBs to make use of this new function. */
 
-CountB = function countBs(str) {
+function countBs(str) {
 
-        for (var i = 0; i < str.length; i++) {
-            return str.length;
+        for (let Count of str) {
+           console.log(count);
         }
-    
-    return 0;
+   
 }
  
-console.log(countB.toUpperCase("bbbbbb"));
+countBs("bbbbBBBjkjdfbb");
 
 // Rewrite the function countBs
 
