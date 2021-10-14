7689fceb81aa67185a76311d1675b78cf493155c
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-14 21:55:26 +0100

    Update sum of a range.js

--- sum of a range.js
+++ sum of a range.js
@@ -3,53 +3,38 @@
 // there are two ways to do this
 // First way
 
+let arr = new Array();    // create a new array.
  function range(start, end) {
-    
-    var { start } = { start: [] };
-    for (var i = 0; i < end; i++) {
-        start.push(i);
+    'use strict';
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
     }
-    return start;
+  return arr;
  }
 
-// Second way: using Destruction
+console.log(range(1, 10));
 
-function range(start, end)
-{
-    start = [];
+//  sum function Declaration
+
+function sum(arr) {
     var sum = 0;
-    for (var i = 0; i < end; i++) {
-        sum += start[i];
-        start.push(i);
-       
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
     }
-    return start;
+    return sum;
 }
 
-console.log(range(1, 10, 2));
-
-function sum() {
-numbers = [2, 13, 5, 9, 6, 10, 7, 3];
-total = 0;
-for (var i = 0; i < numbers.length; i++) {
-    total += numbers[i];
-}
-return total;
-}
-
-console.log(sum());
+console.log(sum(1, 10, 2));
 
 /* Mordify range function to take an optional third argument that indicates the "step" value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4,3, 2] */
 
-function range(start, end, step) {
-    
-    var { start, step } = { start: [], step: 2 };
-    
-        for (var i = 1; i < end; i += 2) {
-            if (step === 2)
-                start.push(i);
-                else {"Write a range function that takes two arguments, start and end, and returns an array containing all the numbers  from start up to (and including) end."}
-        } 
-    return start;
+function ranges(start, end, step) {
+     if (step !== undefined) {
+        for (let i = start; i < end; i += 2) {
+             arr.push(i);   // Gives this [1, 3, 5, 7, 9].
+        }
+    } else return range(1, 10);   // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+return arr;
 }
 
+console.log(ranges(1, 10, 2));
