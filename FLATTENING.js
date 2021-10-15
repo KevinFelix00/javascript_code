5dde7f26acf438ffe77358db99e4c70b2d65260c
xxxSkypper <kevinkila2007@gmail.com>
2021-10-15 14:24:29 +0100

    flattening two arrays

---
+++ FLATTENING.js
@@ -0,0 +1,19 @@
+/* use the reduce method in combination with the concat method to "flatten" an array of arrays into a single array that has all the elements of the original arrays */
+
+function combinationofarrays(arr1, arr2) {
+    'use strict';
+    arr1 = [];
+    arr2 = ["flatten"];
+    let combineAnArrays = arr1.concat(arr2);  // combine arrays.
+    console.log(combineAnArrays);
+    console.log(combineAnArrays.reduce((result) => {
+        for (let elt of combineAnArrays) {   //iterate through the new array combineAnArrays.
+            result = [];  // create a new array.
+            result.push(elt);  // append elt to the new array.
+              
+        }
+        return result;
+    }))
+}
+
+combinationofarrays();
