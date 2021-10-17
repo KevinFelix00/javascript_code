b88784538009453d67fad91e164c0638adcbd9e7
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-17 22:51:47 +0100

    shuffle array string

--- ArrayString.js
+++ ArrayString.js
@@ -2,6 +2,6 @@
 
 'use strict'
 arr = ['ab', 'abacdefgh', 'xyz', 'pqrst'];
-arr.sort((a, b) => b.length - a.length);
+arr.sort((a, b) => b.length - a.length);   // sorting descending order
 
 console.log(arr);
