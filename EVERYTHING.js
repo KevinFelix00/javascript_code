a3fa07fff5a3ca7815790a03eab888ac9d8fe98f
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-20 22:38:16 +0100

    EVERYTHING

--- EVERYTHING.js
+++ EVERYTHING.js
@@ -3,9 +3,10 @@
 // using the loop version
 'use strict';
 function every1(arr, test) {
+    function test() { return true; }
     var arr = [1, 2, 3, 4, 5, 6];
     for (let i = 0; i < arr.length; i++) {
-        if (!test == arr[i]) return false;
+        if (!test(arr[i])) return false;
     }
     return true;
 }
@@ -13,7 +14,7 @@ function every1(arr, test) {
 // using the some method version
 function every2(arr, test) {
     var arr = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-    arr.some((item) => !test )
+    arr.some((item) => !test())
 }
 
 console.log(every1(arr, (item) => (item % 2) == 0));  // check whether any element in the is even
