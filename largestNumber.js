7f31dd877991c9899eab765f5a8ea43c314d8599
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-21 21:21:38 +0100

    largestNumber

---
+++ largestNumber.js
@@ -0,0 +1,14 @@
+/* Find the largest number in an array */
+
+function largest(arr) {
+    arr = [1, 2, 3, 8, 5, 4, 6, 7];
+    var max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(largest(arr))  
