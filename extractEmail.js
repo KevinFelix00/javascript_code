6d8c920fd5f3dc12173a8ae89f5a0a8654158599
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-22 21:40:56 +0100

    extractEmails

---
+++ extractEmail.js
@@ -0,0 +1,10 @@
+/* write a program that an extract email from a string */
+
+//  function for get email id
+function extractEmails(text) {
+    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+}
+
+// test
+
+console.log(extractEmails('hello, my email is: abc@gmail.com'))
