12ec45f02bcfa9fa321af1ccc23a0583c7da0447
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-24 22:48:48 +0100

    remove Element

---
+++ removeElement.js
@@ -0,0 +1,15 @@
+/* Find the index of the array element you want to remove */
+
+var colors = ["red", "blue", "black", "green"];
+
+console.log(colors);
+
+function removeElement(value) {
+    const index = colors.indexOf(value);
+    if (index > -1) {
+        colors.splice(index, 1);
+    }
+}
+
+removeElement("black");
+console.log(colors);
