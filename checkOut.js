8c665ed2a28c65a569e4e4c4442c063f678726ab
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-27 22:48:01 +0100

    check out

--- checkOut.js
+++ checkOut.js
@@ -1,9 +1,11 @@
 /* write a program that check if all values in the arrays are true */
 
+// Test
 var marksArray = [32, 33, 76, 80, 23];
 
+// Check Out
 function checkMarks(mark) {
     return mark > 30;
 }
 
-console.log(marksArray.every(checkMarks));
+console.log(marksArray.every(checkMarks));   // false
