5441059acc7877f104cc852a41d937ffcdde2326
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-10-29 21:22:12 +0100

    reversing an array

--- reversing an array.js
+++ reversing an array.js
@@ -1,9 +1,9 @@
 /* Write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given  as argument by reversing its elements. Neither may use the standard reverse method. */
 
 function reverseArray(arr) {
-      arr.reverse();
-      var rArr = [];
-      rArr.push();
+      arr.reverse();  // reverse the array
+      var rArr = [];  // create an array
+      rArr.push();    // add the reverse element to the new array
     return rArr;
 }
 
@@ -12,7 +12,7 @@ console.log(reverseArray([1, 2, 3, 4, 5]));
 /* second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method. */
 
 function reverseArrayInPlace(arr) {
-     arr.sort((a, b) => b - a);
+     arr.sort((a, b) => b - a);  // sorting the in descending order
       return arr;
 }
 
