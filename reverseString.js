0a2c638b38be3db0cb780103985e8e758e855aaa
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-11-01 21:26:19 +0100

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+/*  Given a string, return a new string with reversed order of character. Make use of Functions and Methods where necessory_
+
+You can code this with any language of your choice! */
+
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello")); 
