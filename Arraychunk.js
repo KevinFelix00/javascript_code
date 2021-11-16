480d3d4658211da41e13fafac19defae0802d4a3
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-11-16 23:47:29 +0100

    Array Chunk

--- Arraychunk.js
+++ Arraychunk.js
@@ -3,9 +3,9 @@ const chunk = function (array, size){
         return [];
     }
     const head = array.slice(0, size);
-    const tail = array.slice(size);
+    const tail = array.slice(size);  // Pull a copy of a portion of an array
     
     return [head, ...chunk(tail, size)];
 };
 
-console.log(chunk([1,2,3,4,5,6], 2));
+console.log(chunk([1,2,3,4,5,6], 2));  // [[1,2],[3,4],[5,6]]
