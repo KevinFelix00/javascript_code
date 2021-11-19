a83eec976373516dbe54f46a609ac9933900dd4c
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-11-19 20:56:06 +0100

    Anagram

--- Anagram.js
+++ Anagram.js
@@ -1,4 +1,6 @@
 // [November]/[Day 17]/[Javascript]/{Anagram.js}
+/* Check to see if two provided strings are anagrams of each other. One String is an anagram of another if it uses the same character in the same quantity. Only consider characters, not spaces or punctuation. Consider capital letters in your program. Make use of Functions and Methods where necessory */
+
 function anagram(str1, str2) {
     if (str1.lenth !== str2.lenth) return false;
     else {
