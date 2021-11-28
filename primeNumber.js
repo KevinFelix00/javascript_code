0c0c7a0bb651569864a3ae42471a8b047dd3a987
Kevin <77277779+xxxSkypper@users.noreply.github.com>
2021-11-28 18:18:18 +0100

    PrimeNumber

--- primeNumber.js
+++ primeNumber.js
@@ -5,8 +5,8 @@ const isPrime = num => {
     
 }
 
-console.log(isPrime(0));
-console.log(isPrime(1));
-console.log(isPrime(2));
-console.log(isPrime(3));
-console.log(isPrime(4));
+console.log(isPrime(0));  // false
+console.log(isPrime(1));  // false
+console.log(isPrime(2));  // true
+console.log(isPrime(3));  // true
+console.log(isPrime(4));  // false
